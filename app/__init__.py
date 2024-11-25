9771f4f7aa38d5223c02d97bba0ae3ebe778b6bb
Tomas Fiure <tf2503@columbia.edu>
2024-11-25 17:23:33 -0500

    API setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 #User-profile/app/init.py
 from flask import Flask
 from flask_cors import CORS
-from .routers import user_blueprint
+from .routers import user
 from .database import engine, Base
 
 def create_app():
@@ -9,7 +9,7 @@ def create_app():
     CORS(app)
 
     # Register user blueprint
-    app.register_blueprint(user_blueprint)
+    app.register_blueprint(user.user_blueprint)
 
     # Create database tables if they don't exist
     Base.metadata.create_all(bind=engine)
