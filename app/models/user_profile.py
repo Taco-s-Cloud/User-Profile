c4c1ea99ac124f6c45a334bd080a96d7663709a0
tomasfiure <123346779+tomasfiure@users.noreply.github.com>
2024-12-05 13:44:11 -0500

    Update user_profile.py

--- app/models/user_profile.py
+++ app/models/user_profile.py
@@ -7,7 +7,6 @@ class User(Base):
     user_id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
-    full_name = Column(String, nullable=True)
     registration_date = Column(DateTime, nullable=False)
     first_name = Column(String, nullable=True)
     last_name = Column(String, nullable=True)
